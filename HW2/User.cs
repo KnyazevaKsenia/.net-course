d520f059f5ba2ad00d61880e74e9cdbf512a325b
Ksenia <kn_ksusha@mail.ru>
2025-09-13 16:53:13 +0300

    Users manager added

---
+++ HW2/User.cs
@@ -0,0 +1,16 @@
+﻿namespace HW2;
+
+public class User
+{
+    public User(int id, string username, int age, string password)
+    {
+        Id = id;
+        Username = username;
+        Age = age;
+        Password = password;
+    }
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public int Age { get; set; }
+    public string Password { get; set; }
+}
---
+++ HW2/UsersManager.cs
@@ -0,0 +1,50 @@
+﻿namespace HW2;
+
+public static class UsersManager
+{
+    private static List<User?> Users{ get; set; } = new List<User?>();
+        
+    public static (bool Result, string Message) AddUser(string username, string password, int age)
+    {
+        if (Users.Find(u => u.Username == username) != null)
+        {
+            var newUser = new User(Users.Count + 1, username, age, password);
+            Users.Add(newUser);
+            return (true, $"User {username} added.");
+        }
+        return (false, $"User {username} already exist.");
+    }
+    
+    public static (bool Result, string Message) EditUserName(string oldUsername, string newUsername)
+    {
+        var oldUser = Users.Find(u => u.Username == oldUsername);
+        if (Users.Find(u => u.Username== newUsername) == null && oldUser!=null)
+        {
+            oldUser.Username = newUsername;
+            return (true, $"User {oldUsername} updated.");
+        }
+        return (false, "User with this name already exist.");
+    }
+    
+    public static (bool Result, string Message) DeleteUser(int userId)
+    {
+        User? userToRemove = Users.Find(user => user != null && user.Id == userId);
+        if (userToRemove != null)
+        {
+            Users.Remove(userToRemove);
+            return (true, $"User {userToRemove.Username} deleted.");
+        }
+        return (false, $"User does not exist.");
+    }
+    
+    public static (bool Result, string Message) AutorizeUser(string username, string password)
+    {   
+        User user = Users.Find(u => u.Username == username);
+        if (user.Password == password)
+        {
+            return (true, $"User {username} is autorized.");
+        }
+        
+        return (false, $"Wrong password.");
+    }
+}
